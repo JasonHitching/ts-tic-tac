cba2de095d3595947368f8d6e176dd1c5ca2a451
Jason Hitching <jasonhitching1994@gmail.com>
2020-11-07 16:36:51 +0000

    implemented: winning functionality
    - A user can now win with a message being displayed

--- src/components/GameBoard/Board.tsx
+++ src/components/GameBoard/Board.tsx
@@ -1,23 +1,44 @@
 import React, { useState } from "react";
-import { Square } from '../BoardSquare/Square';
-import '../css/styling.css'
+import { Square } from "../BoardSquare/Square";
+import "../css/styling.css";
 
 export const Board: React.FC = () => {
-  
-  const [squares, setValue] = useState(Array(9).fill(null));
+  const [squares, setValue] = useState<string[]>(Array(9).fill(""));
+  const [xIsNext, setNext] = useState<boolean>(true);
 
-    const handleClick = (i: number) => {
-        const squareVals: string[] = squares.slice();
-        squareVals[i] = "X";
-        setValue(squareVals);
+  /** Function to handle when the square class triggers onClick
+   *  i.e. a square has been clicked
+   *  params: num of square clicked
+   */
+  const handleClick = (i: number) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
+    squares[i] = xIsNext ? "X" : "0";
+    const squareVals: string[] = squares.slice();
+    setValue(squareVals);
+    setNext(!xIsNext);
   };
 
+  /** Render a square component on the page
+   *  renders with an initial value and is passed a handleClick function
+   *  when the square component triggers the handleClick function it's 
+   *  changed an re-rendered
+   */
   const renderSquare = (i: number) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
-  
-  const status = "Next player: X";
 
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? 'X' : '0');
+  }
+  
   return (
     <div>
       <div className="status">{status}</div>
@@ -38,4 +59,25 @@ export const Board: React.FC = () => {
       </div>
     </div>
   );
+
+  function calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 };
