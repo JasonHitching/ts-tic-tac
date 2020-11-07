d27593284c1efd07647f272d0c6e4a13e3851073
Jason Hitching <jasonhitching1994@gmail.com>
2020-11-07 07:47:40 +0000

    implemented: Hook to modify the board state upon click

--- src/components/BoardSquare/Square.tsx
+++ src/components/BoardSquare/Square.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/styling.css'
 
 interface IProps {
@@ -6,10 +6,12 @@ interface IProps {
 }
 
 export const Square = (props: IProps) => {
+    const [value, setValue] = useState('');
+
     return (
         <div>
-            <button className="square" onClick={() => alert('click')} >
-                {props.value}
+            <button className="square" onClick={() => setValue('X')} >
+                {value}
             </button>
         </div>
     )
